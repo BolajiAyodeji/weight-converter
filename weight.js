954c15af4e4b94dea571c7405a1147bc70fffdbb
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-23 15:14:34 +0100

    test :octocat:

---
+++ weight.js
@@ -0,0 +1,8 @@
+  let grams = document.getElementById('gOutput');
+  grams.innerHTML = lbs/0.0022046;
+
+  let kiloGrams = document.getElementById('kgOutput');
+  kiloGrams.innerHTML = lbs/2.2046;
+
+  let ounces = document.getElementById('ozOutput');
+  ounces.innerHTML = lbs*16;
