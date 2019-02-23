e29b6ab4eb71a7a50fd6f4eeec14c386c00c28cb
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-23 15:14:02 +0100

    setup converter

---
+++ docs/js/app.js
@@ -0,0 +1,19 @@
+const input = document.getElementById('lbsInput');
+const output = document.getElementById('output');
+
+output.style.visibility = 'hidden';
+
+input.addEventListener('input', function(e) {
+  let lbs = e.target.value;
+
+  output.style.visibility = 'visible';
+
+  let grams = document.getElementById('gOutput');
+  grams.innerHTML = lbs/0.0022046;
+
+  let kiloGrams = document.getElementById('kgOutput');
+  kiloGrams.innerHTML = lbs/2.2046;
+
+  let ounces = document.getElementById('ozOutput');
+  ounces.innerHTML = lbs*16;
+});
