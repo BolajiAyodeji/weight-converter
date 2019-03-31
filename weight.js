a2277b971c221ad45761696e6bdc33a76cc92bc5
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-03-31 14:58:30 +0100

    update UI

--- weight.js
+++
@@ -1,8 +0,0 @@
-  let grams = document.getElementById('gOutput');
-  grams.innerHTML = lbs/0.0022046;
-
-  let kiloGrams = document.getElementById('kgOutput');
-  kiloGrams.innerHTML = lbs/2.2046;
-
-  let ounces = document.getElementById('ozOutput');
-  ounces.innerHTML = lbs*16;
